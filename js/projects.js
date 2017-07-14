d784e97417116528ed9dccda33cace0dbb5e2935
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-07-13 20:25:29 -0500

    web_013
    changes to css and js for projects page

---
+++ js/projects.js
@@ -0,0 +1,20 @@
+/* project.html JavaScript*/
+var images = document.getElementsByClassName("imageStyles");
+
+for (var x = 0; x < images.length; x++){
+	var img = images[x];
+	var imgID = document.getElementById("img01");
+	var captionText = document.getElementById("text");
+	img.onclick = function(){
+		fullImg.style.display = "block";
+		imgID.src = this.src;
+		captionText.innerHTML = this.alt;
+	}
+}
+
+var fullImg = document.getElementById('fullImage');
+var span = document.getElementsByClassName("exit")[0];
+
+span.onclick = function() { 
+    fullImg.style.display = "none";
+}
