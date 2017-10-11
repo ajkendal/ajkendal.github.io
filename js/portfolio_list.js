9d0cf17c53c4fe7d96843d46713eb57dc1a408b0
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-10-10 20:37:00 -0500

    web_038
    more changes, not finished. Need to implement the full page feature. then update all projects, then add media quereiss

---
+++ js/portfolio_list.js
@@ -0,0 +1,22 @@
+var projectList = { "projects": [
+    {
+        header: "Text Header",
+        year: 2017,
+        image: "../img/project_img/WCR_Poster_2015.png",
+        alt: "Test Project",
+        descr: "Test Descreption",
+        imageLink: "<a href='#' class='fullImageLink'>Full Image</a> ",
+        projectLink: " | <a class='projectExternal_Links' href='#' target='_blank'>See Project</a>",
+        githubLink: " | <a class='projectExternal_Links' href='#' target='_blank'>See Repository</a>"
+    },
+    {
+        header: "Text Header2",
+        year: 2017,
+        image: "../img/project_img/WCR_Poster_2015.png",
+        alt: "Test Project",
+        descr: "Test Descreption",
+        imageLink: "<a href='#' class='fullImageLink'>Full Image</a> ",
+        projectLink: " | <a class='projectExternal_Links' href='#' target='_blank'>See Project</a>",
+        githubLink: " "
+    }
+]};
