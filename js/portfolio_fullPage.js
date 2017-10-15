22eda430face037eec4d979c2fb63bdade453067
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-10-15 09:14:33 -0500

    web_038
    Changes for Issue #76 ,

--- js/portfolio_fullPage.js
+++ js/portfolio_fullPage.js
@@ -1,5 +1,20 @@
-function openFullImage(image) {
+function openFullImage(imageString) {
     "use strict";
-    alert(image);
-   
+    var img = document.createElement('img');
+    img.src = imageString;
+    
+    document.getElementById('centeredIDImage').appendChild(img);
+    
+    document.getElementById('imageOverlay').style.width = "100%";
+}
+
+function closeFullImage() {
+    "use strict";
+    
+    var clearImage = document.getElementById('centeredIDImage');
+    
+    while (clearImage.hasChildNodes()) {   
+        clearImage.removeChild(clearImage.firstChild);
+    }
+    document.getElementById('imageOverlay').style.width = "0%";
 }
