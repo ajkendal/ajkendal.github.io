3e460cb90ef1ba34adf36491774fbbb9df4c039a
Amanda Kendal-Brown <ajkendal@neiu.edu>
2020-06-03 15:55:24 -0500

    updates

--- js/resume.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-var percBars = document.getElementsByClassName("bar");
-
-for (var x = 0; x < percBars.length; x++){	
-	percBars[x].style.width = lengths[x].toString() + '%';
-}
