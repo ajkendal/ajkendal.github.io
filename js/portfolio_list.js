364298b876a27d07c29ba422f9c3ceba9f608f4c
Amanda J Kendal-Brown <15990675+ajkendal@users.noreply.github.com>
2021-01-23 10:47:27 -0600

    Update portfolio_list.js

--- js/portfolio_list.js
+++ js/portfolio_list.js
@@ -203,7 +203,7 @@ var projectList = { "projects": [
         spacer1: " ",
         projectLink: " ",
         spacer2: " | ",
-        githubLink: "<a class='projectExternal_Links' href='https://github.com/ajkendal/words-with-friends-exercise' target='_blank'>Repositary</a>"
+        githubLink: "<a class='projectExternal_Links' href='https://github.com/ajkendal/words-with-friends-exercise' target='_blank'>Repository</a>"
     },
     {
         header: "Tank Rampage",
@@ -520,20 +520,20 @@ var projectList = { "projects": [
         spacer2: " | ",
         githubLink: "<a class='projectExternal_Links' href='https://spark.adobe.com/page/GC4hPudnD490b' target='_blank'>Mood Board</a>"
     },
-	{
-		header: "Profile Card Component",
-		year: 2021,
-		city: "Chicago, IL",
-		image: imageSrc[37],
-		alt: "HTML",
-		imageZoom: "40%",
-		type: "Coding Challenege",
-		descr: "The challenege, from Frontend Mentor, was to build out a profile card component and get it looking as close to the design as possible using HTML and CSS.",
-		imageLink: "<a href='javascript:void(0)' onclick='openFullImage(imageSrc[37])'>Full Image</a>",
-		spacer1: " | ",
-		projectLink: "<a class='projectExternal_Links' href='https://ajkendal.github.io/profile-card-component/' target='_blank'>See Project</a>",
-		spacer2: " | ",
-		githubLink: "<a class='projectExternal_Links' href='https://github.com/ajkendal/profile-card-component' target='_blank'>GitHub</a>"
+    {
+	header: "Profile Card Component",
+	year: 2021,
+	city: "Chicago, IL",
+	image: imageSrc[37],
+	alt: "HTML",
+	imageZoom: "40%",
+	type: "Coding Challenege",
+	descr: "The challenege, from Frontend Mentor, was to build out a profile card component and get it looking as close to the design as possible using HTML and CSS.",
+	imageLink: "<a href='javascript:void(0)' onclick='openFullImage(imageSrc[37])'>Full Image</a>",
+	spacer1: " | ",
+	projectLink: "<a class='projectExternal_Links' href='https://ajkendal.github.io/profile-card-component/' target='_blank'>See Project</a>",
+	spacer2: " | ",
+	githubLink: "<a class='projectExternal_Links' href='https://github.com/ajkendal/profile-card-component' target='_blank'>Repository</a>"
     }
 ]};
 
