f917762c84f1b5ee83776588418bd2ab61657e0a
Amanda Kendal-Brown <amanda.kendal-brown@automox.com>
2024-01-08 14:30:19 -0700

    tresting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ajkendal' title`, () => {
+  it(`should have the 'ajkendal.github.io' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ajkendal');
+    expect(app.title).toEqual('ajkendal.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ajkendal');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ajkendal.github.io');
   });
 });
